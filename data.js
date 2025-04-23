6dc39a737d3d296bd7d2d352f56475bebec44226
Sylvie Kerr <kerrsylvie@gmail.com>
2025-04-23 12:51:12 -0700

    Update data.js

--- data.js
+++ data.js
@@ -214,7 +214,7 @@ const data = {//width/height x/y are swapped in here.. oops? idk it doesnt rlly 
             creator: "@_scrlk"
         },
         {
-            title: "grape+onion 6orb",
+            title: "grape+onion",
             equip: "Obsidian +6",
             code: "`/2.2/gga6/-cd2bd2dd2f-erbbdof-grbfdof-iobodof-kc3b/-bifihqjqlc0nqpqritivd3x-dc2dd1fshojoloporstd3vc2x-fiffhqjqlqpqrftiv-hd1fshojolc0noporstd3v-jd1difihqjqlqpqritivd3x-lc2dd1fshojoloporstd3vc2x-nifihqjqlc0nqpqritiv-pd1fshojoloporstd3v-rd1diffhqjqlqpqrftiv-td1difshojole1noporstivd3x-vd1difihqjqlqpqritiv-xc3fd0lc1p/-brbrd-dfbbd-frbrd-hd0bd0d//-clb-ela/-bd1brdbfrh-dd1brdffrh-fd1brdrfrh-hc1d/`",
             creator: "@tsukiglitchhunter + @syiv"
